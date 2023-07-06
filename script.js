aa00efaece45ac26422ba42444d36bc90125cb28
f6c5 <cicekf256@gmail.com>
2023-07-06 16:36:25 +0300

    ...

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-console.log("JavaScript dosyası çalışıyor!");
-
 function sayHello() {
   console.log("Merhaba dünya!");
 }
 
-sayHello();
+function addButton() {
+  var button = document.createElement("button");
+  button.textContent = "Merhaba de";
+  button.addEventListener("click", sayHello);
+  document.body.appendChild(button);
+}
+
+addButton();
